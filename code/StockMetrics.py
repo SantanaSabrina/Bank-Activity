c55823252bb8adf4f24475286f2a78ac55c244df
Sabrina Santana <sabrina.santana.90@gmail.com>
2023-10-30 20:02:40 -0400

    Final Submission

--- code/StockMetrics.py
+++ code/StockMetrics.py
@@ -18,16 +18,33 @@ class StockMetrics(StockData):
         """
         averages = []
         for row in self.data:
-            ...
-
+            
+            new_row = [float(val) for val in row[1:] if val != "" and val != " "]
+            x = stats.mean(new_row)
+            averages.append(round(x, 3))
+            print (new_row)
         return averages
 
     def median02(self):
         """pt2
         """
-        ...
+        medians = []
+        for row in self.data:
+
+            new_row = [float(val) for val in row[1:] if val != "" and val != " "]
+            x = stats.median(new_row)
+            medians.append(x)
+        return medians
 
     def stddev03(self):
         """pt3
         """
-        ...
+        stdevs = []
+        for row in self.data: 
+
+            new_row = [float(val) for val in row[1:] if val != "" and val != " "]
+            x = stats.stdev(new_row)
+            stdevs.append(round(x, 3))
+        return stdevs
+
+
